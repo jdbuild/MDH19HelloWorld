1fb0a95eb9d104fdcf2190bad034d642438f260d
jdbuild <jakob.doppler@gmail.com>
2020-01-11 09:42:21 +0100

    Update app.js
    cool change

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(cookieParser())
 app.set('port', (process.env.PORT || 3000));
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Jakob!'))
 
 app.get('/cookies', (req, res) => {
     // Read cookies
